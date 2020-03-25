d009b1dc4ded140b683fff598d5b88dc5e0bf1f3
Chiffilin <dimaaitbaev42@gmail.com>
2020-03-25 11:04:36 +0200

    Добавьте файлы проекта.

---
+++ Lab3.Easy/Lab3.Easy.cpp
@@ -0,0 +1,32 @@
+﻿
+
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main()
+{
+     
+    char s[10];
+    cout << "Enter 10 symbols:\n";
+    int N = 10;
+    for (int i = 0; i < N; i++)
+        cin >> s[i];
+
+    for (int i = 0; i < N; i++)
+    {
+        if (isdigit(s[i])) 
+        {
+            s[i] = '*';
+            cout << s[i] << " ";
+        }
+        else
+        {
+            s[i] = '*';
+            cout << s[i] << " ";
+        }
+    }
+    cout << "\n";
+    system("pause");
+    return 0;
+}
